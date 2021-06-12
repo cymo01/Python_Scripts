9ae9bb6f9c0533153090430c234f98e94bfcb2e8
Kem White <kemwhite@outlook.com>
2021-06-12 07:57:23 -0400

    Update

--- SimplePy01/SimplePy01.py
+++ SimplePy01/SimplePy01.py
@@ -9,6 +9,7 @@
 import numpy as np
 import sys
 import pickle
+import random
 print("Hello, world?")
 # Create 2 new lists height and weight
 print("Hello, world!")
@@ -31,3 +32,6 @@ print(np_height)
 print(np_weight)
 
 print(np.pi)
+
+random.gauss(0,1)
+print(random.gauss(0,1))
